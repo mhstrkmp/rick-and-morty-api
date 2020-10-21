209db8e200ec55dc7f03e78b24d4d0d192145103
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-21 12:11:01 +0200

    Add api functions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./app.css";
+import Character from "./components/Character";
+import { getCharacterById } from "./utils/api";
 import Header from "./components/Header";
 import { createElement } from "./utils/elements";
 
 function App() {
   const header = Header();
+  const main = createElement("main");
 
-  const main = createElement("main", {
-    innerText: "👋",
-  });
+  async function getCharacters() {
+    const character1 = await getCharacterById(1);
+    main.append(
+      Character({
+        name: character1.name,
+        imgSrc: character1.image,
+      })
+    );
+  }
 
+  getCharacters();
   const container = createElement("div", { children: [header, main] });
   return container;
 }
