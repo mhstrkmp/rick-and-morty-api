ebc4c930442928c891ecf218abd1a66a9e6b4f6f
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-21 21:06:01 +0200

    Add some animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import "./header.css";
 import { createElement } from "../utils/elements";
+import antennaRick from "../assets/antennaRickNoBg.png";
 
 function Header() {
   const siteTitle = createElement("h1", {
     innerText: "Rick & Morty",
   });
+  const siteDescription = createElement("h2", {
+    innerText: "What the API?",
+  });
+  const siteImage = createElement("img", {
+    className: "header__image",
+    src: antennaRick,
+    alt: "Antenna Rick",
+  });
 
   const header = createElement("header", {
     className: "header",
-    children: [siteTitle],
+    children: [siteTitle, siteDescription, siteImage],
   });
   return header;
 }
